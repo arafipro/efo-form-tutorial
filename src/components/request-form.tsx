3eae3a268d03aa3a3453c040d4abb781c3a52aa8
Hiroki Chiyoda <contact@arafipro.com>
2024-12-08 15:56:46 +0900

    郵便番号からAPIを使って住所を取得
    https://github.com/ttskch/jp-postal-code-api

--- src/components/request-form.tsx
+++ src/components/request-form.tsx
@@ -10,7 +10,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import {
   InputOTP,
   InputOTPGroup,
@@ -21,30 +20,30 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const requestFormSchema = z.object({
-  fullName: z
-    .string()
-    .trim()
-    .min(1, { message: "名前を入力してください" })
-    .max(50)
-    .regex(/^[\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}ー\s]+$/u, {
-      message: "名前は漢字、ひらがな、カタカナで入力してください",
-    }),
-  nameKana: z
-    .string()
-    .trim()
-    .min(1, { message: "よみがなを入力してください" })
-    .max(50)
-    .regex(/^[\p{Script=Hiragana}ー\s]+$/u, {
-      message: "よみがなはひらがなで入力してください",
-    }),
-  email: z
-    .string()
-    .trim()
-    .min(1, { message: "メールアドレスを入力してください" })
-    .max(50)
-    .regex(/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/u, {
-      message: "メールアドレスを入力してください",
-    }),
+  // fullName: z
+  //   .string()
+  //   .trim()
+  //   .min(1, { message: "名前を入力してください" })
+  //   .max(50)
+  //   .regex(/^[\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}ー\s]+$/u, {
+  //     message: "名前は漢字、ひらがな、カタカナで入力してください",
+  //   }),
+  // nameKana: z
+  //   .string()
+  //   .trim()
+  //   .min(1, { message: "よみがなを入力してください" })
+  //   .max(50)
+  //   .regex(/^[\p{Script=Hiragana}ー\s]+$/u, {
+  //     message: "よみがなはひらがなで入力してください",
+  //   }),
+  // email: z
+  //   .string()
+  //   .trim()
+  //   .min(1, { message: "メールアドレスを入力してください" })
+  //   .max(50)
+  //   .regex(/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/u, {
+  //     message: "メールアドレスを入力してください",
+  //   }),
   postalCode: z
     .string()
     .min(7, { message: "郵便番号を入力してください" })
@@ -58,9 +57,9 @@ export default function RequestForm() {
   const form = useForm<z.infer<typeof requestFormSchema>>({
     resolver: zodResolver(requestFormSchema),
     defaultValues: {
-      fullName: "",
-      nameKana: "",
-      email: "",
+      // fullName: "",
+      // nameKana: "",
+      // email: "",
       postalCode: undefined,
     },
   });
@@ -70,7 +69,7 @@ export default function RequestForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
+        {/* <FormField
           control={form.control}
           name="fullName"
           render={({ field }) => (
@@ -120,7 +119,7 @@ export default function RequestForm() {
               <FormMessage />
             </FormItem>
           )}
-        />
+        /> */}
         <FormField
           control={form.control}
           name="postalCode"
@@ -132,7 +131,27 @@ export default function RequestForm() {
               </FormLabel>
               <FormDescription>例：000-0000</FormDescription>
               <FormControl>
-                <InputOTP maxLength={7} {...field} inputMode="numeric">
+                <InputOTP
+                  maxLength={7}
+                  {...field}
+                  inputMode="numeric"
+                  onChange={async (value) => {
+                    field.onChange(value); // フィールドの値を更新
+                    const postalCode = value.replace(/-/g, ""); // ハイフンを削除
+                    if (postalCode.length === 7) {
+                      // 住所取得APIを呼び出す
+                      const response = await fetch(
+                        `https://jp-postal-code-api.ttskch.com/api/v1/${postalCode}.json`
+                      );
+                      const data = await response.json();
+                      // 取得した住所をフォームに設定する処理を追加
+                      // 例: setValue('address', data.address);
+                      console.log(
+                        `${data.addresses[0].ja.prefecture}${data.addresses[0].ja.address1}${data.addresses[0].ja.address2}`
+                      );
+                    }
+                  }}
+                >
                   <InputOTPGroup>
                     <InputOTPSlot className="bg-white" index={0} />
                     <InputOTPSlot className="bg-white" index={1} />
