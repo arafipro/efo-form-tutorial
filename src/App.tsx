aef9f1cbdd3073fb673af164916b2ed2965259be
Hiroki Chiyoda <contact@arafipro.com>
2024-12-08 10:57:13 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="flex flex-col min-h-screen items-center">
+      <h1 className="text-3xl font-bold">EFO フォーム Tutorial</h1>
+    </main>
+  );
+}
